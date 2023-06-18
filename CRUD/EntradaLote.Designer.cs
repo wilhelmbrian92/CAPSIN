3a11fe9c0741a62440e7668df609a502e836ca17
brian wilhelm <wilhelmbrian92@hotmail.com>
2023-06-18 00:06:20 -0300

    se agrega campo proveedor en input y campo Presentacion para la entrada de productos

--- CRUD/EntradaLote.Designer.cs
+++ CRUD/EntradaLote.Designer.cs
@@ -42,13 +42,16 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.txtCaja = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblProducto
             // 
-            this.lblProducto.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lblProducto.AutoSize = true;
             this.lblProducto.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblProducto.ForeColor = System.Drawing.SystemColors.ControlDark;
@@ -65,7 +68,7 @@
             this.cboxProductos.FormattingEnabled = true;
             this.cboxProductos.Location = new System.Drawing.Point(97, 45);
             this.cboxProductos.Name = "cboxProductos";
-            this.cboxProductos.Size = new System.Drawing.Size(446, 24);
+            this.cboxProductos.Size = new System.Drawing.Size(454, 24);
             this.cboxProductos.TabIndex = 2;
             this.cboxProductos.SelectedIndexChanged += new System.EventHandler(this.cboxProductos_SelectedIndexChanged);
             // 
@@ -86,7 +89,6 @@
             // 
             // lblCantidad
             // 
-            this.lblCantidad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lblCantidad.AutoSize = true;
             this.lblCantidad.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblCantidad.ForeColor = System.Drawing.SystemColors.ControlDark;
@@ -98,7 +100,6 @@
             // 
             // lblVencimiento
             // 
-            this.lblVencimiento.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lblVencimiento.AutoSize = true;
             this.lblVencimiento.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblVencimiento.ForeColor = System.Drawing.SystemColors.ControlDark;
@@ -114,7 +115,7 @@
             this.txtCantidad.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtCantidad.Location = new System.Drawing.Point(97, 79);
             this.txtCantidad.Name = "txtCantidad";
-            this.txtCantidad.Size = new System.Drawing.Size(446, 22);
+            this.txtCantidad.Size = new System.Drawing.Size(454, 22);
             this.txtCantidad.TabIndex = 10;
             this.txtCantidad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCantidad_KeyPress);
             // 
@@ -124,7 +125,7 @@
             this.dtpVencimiento.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dtpVencimiento.Location = new System.Drawing.Point(97, 111);
             this.dtpVencimiento.Name = "dtpVencimiento";
-            this.dtpVencimiento.Size = new System.Drawing.Size(446, 22);
+            this.dtpVencimiento.Size = new System.Drawing.Size(454, 22);
             this.dtpVencimiento.TabIndex = 11;
             // 
             // rbtnRemediar
@@ -132,7 +133,7 @@
             this.rbtnRemediar.AutoSize = true;
             this.rbtnRemediar.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rbtnRemediar.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.rbtnRemediar.Location = new System.Drawing.Point(6, 53);
+            this.rbtnRemediar.Location = new System.Drawing.Point(280, 4);
             this.rbtnRemediar.Name = "rbtnRemediar";
             this.rbtnRemediar.Size = new System.Drawing.Size(88, 20);
             this.rbtnRemediar.TabIndex = 5;
@@ -146,7 +147,7 @@
             this.rbtnSecretaria.AutoSize = true;
             this.rbtnSecretaria.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rbtnSecretaria.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.rbtnSecretaria.Location = new System.Drawing.Point(6, 25);
+            this.rbtnSecretaria.Location = new System.Drawing.Point(167, 4);
             this.rbtnSecretaria.Name = "rbtnSecretaria";
             this.rbtnSecretaria.Size = new System.Drawing.Size(92, 20);
             this.rbtnSecretaria.TabIndex = 4;
@@ -169,19 +170,23 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.lblProveedor);
             this.panel1.Controls.Add(this.rbtnSecretaria);
             this.panel1.Controls.Add(this.rbtnRemediar);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panel1.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.panel1.Location = new System.Drawing.Point(0, 444);
+            this.panel1.Location = new System.Drawing.Point(0, 373);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(563, 81);
+            this.panel1.Size = new System.Drawing.Size(563, 152);
             this.panel1.TabIndex = 13;
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.txtCaja);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.cboxProductos);
@@ -206,20 +211,58 @@
             this.txtCaja.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtCaja.Location = new System.Drawing.Point(97, 144);
             this.txtCaja.Name = "txtCaja";
-            this.txtCaja.Size = new System.Drawing.Size(446, 22);
+            this.txtCaja.Size = new System.Drawing.Size(454, 22);
             this.txtCaja.TabIndex = 13;
             // 
             // label1
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.label1.Location = new System.Drawing.Point(28, 142);
+            this.label1.Location = new System.Drawing.Point(37, 144);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(54, 16);
             this.label1.TabIndex = 12;
             this.label1.Text = "ID Caja:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(97, 177);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(454, 22);
+            this.textBox1.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.Location = new System.Drawing.Point(3, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Presentación:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(53, 105);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(315, 22);
+            this.textBox2.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Otros:";
             // 
             // Registrar_ENTRADA
             // 
@@ -260,5 +303,9 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox txtCaja;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
     }
 }
