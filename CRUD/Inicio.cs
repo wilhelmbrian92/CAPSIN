87ef7c031c1f4d9fbcb4e6af07b39e95c5088828
brian wilhelm <wilhelmbrian92@hotmail.com>
2023-06-19 21:27:51 -0300

    se saca la imagen de los pulmones debido a imagen caida

--- CRUD/Inicio.cs
+++ CRUD/Inicio.cs
@@ -17,7 +17,7 @@ namespace CRUD
         {
             InitializeComponent();
             picLogoBrown.ImageLocation = "https://licencias.transito.brown.gob.ar/assets/images/brown-logo.png";
-            picPulmones.ImageLocation = "https://img3.freepng.es/dy/9969a45ece9a773020572e4a08a2d6b2/L0KzQoG3U8AzN6dnkZH9cnHxg8HokvVvfF5xjdDwcz3mf8fwhL1kd6N0htM2dnn1hcS0hPl0bZJ4fZ8AZUXnQoLrhvI2QWNmS5CDMES3RIqAUsE2QGQ2TqMDMkO6RISATwBvbz==/transparent-lungs-covid-corona-virus-disease-5e5d21dfb592a3.8044497215831618237437.png";
+            
         }
 
 
